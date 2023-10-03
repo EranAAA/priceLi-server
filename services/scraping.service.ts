ad3001779254304142caf0e24645d486028f2d86
Eran Avichzer <eranavichzer@Erans-Mac-mini.local>
2023-10-03 09:20:45 +0300

    convert relavent code to typescript

--- services/scraping.service.ts
+++ services/scraping.service.ts
@@ -1,143 +1,134 @@
-const puppeteer = require('puppeteer')
-const logger = require('./logger.service')
+const puppeteer = require("puppeteer")
+const logger = require("./logger.service")
 
-const fs = require('fs')
-const path = require("path");
+const fs = require("fs")
+const path = require("path")
 
 const USER_NAME = "RamiLevi"
+const URL_PATH = "https://url.retail.publishedprices.co.il/login"
 
-// URL
-const urlRamiLevi = 'https://url.retail.publishedprices.co.il/login'
+// const _getRamiLeviFile = async path => {
+// 	const filePath = `./files/${path.title}`
+// 	const URL = path.href + path.title
 
-// RamiLevi
+// 	// console.log('File Path: ', filePath)
+// 	// console.log('Full URL: ', 'https://url.retail.publishedprices.co.il' + path.href)
+
+// 	try {
+// 		const file = fs.createWriteStream(filePath)
+// 		// const request = https.get(URL, (response) => {
+// 		//    response.pipe(file)
+// 		// })
+
+// 		// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+// 		console.log(URL)
+// 		request
+// 			.get(URL)
+// 			.on("error", err => {
+// 				console.error("******** error: ********", err)
+// 			})
+// 			.pipe(file)
+
+// 		console.log("File SAVED")
+// 	} catch (error) {
+// 		console.log("******** ERROR: ********", error)
+// 	}
+// }
+
+// const _renameFile = () => {
+// 	const folderPath = "./files"
+
+// 	let filesArr = fs.readdirSync(folderPath)
+// 	filesArr.forEach(file => {
+// 		let fullPath = path.join(folderPath, file)
+// 		let fileExtension = path.extname(file)
+// 		let fileName = path.basename(file, fileExtension)
+
+// 		if (fileExtension === ".crdownload") {
+// 			console.log(file)
+// 			console.log(path.extname(file))
+// 			let newFileName = fileName
+// 			try {
+// 				fs.renameSync(fullPath, path.join(folderPath, newFileName))
+// 			} catch (error) {
+// 				console.error(error)
+// 			}
+// 		}
+// 	})
+// }
+
+// const _delay = (time: number) => {
+// 	return new Promise(function (resolve) {
+// 		setTimeout(resolve, time)
+// 	})
+// }
+
 const getRamiLeviPuppeteer = async (fileName: string) => {
+	const browser = await puppeteer.launch({
+		ignoreHTTPSErrors: true,
+	})
+	const page = await browser.newPage()
 
-   const browser = await puppeteer.launch({
-      ignoreHTTPSErrors: true
-   })
-   const page = await browser.newPage()
+	const client = await page.target().createCDPSession()
+	await client.send("Page.setDownloadBehavior", { behavior: "allow", downloadPath: "./files" })
+	logger.info("Set Download Behavior")
 
-   const client = await page.target().createCDPSession()
-   await client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: './files' })
-   logger.info('Set Download Behavior')
+	await page.goto(URL_PATH)
+	logger.info("Enter Rami Levi page successefully")
 
-   await page.goto(urlRamiLevi)
-   logger.info('Enter Rami Levi page successefully')
+	await page.type("#username", USER_NAME)
+	logger.info("insert UserName: ", USER_NAME)
 
-   await page.type('#username', USER_NAME)
-   logger.info('insert UserName: ', USER_NAME)
+	await Promise.all([page.click("#login-button"), page.waitForNavigation({ waitUntil: "networkidle0" })])
+	logger.info("Submitted form")
 
-   await Promise.all([
-      page.click('#login-button'),
-      page.waitForNavigation({ waitUntil: 'networkidle0' }),
-   ])
-   logger.info('Submitted form')
+	logger.info("Searching...")
+	await page.type(".form-control", fileName, { delay: 1000 })
 
-   logger.info('Searching...')
-   await page.type('.form-control', fileName, { delay: 1000 })
+	const data = await page.evaluate(() => {
+		return Promise.resolve({
+			href: (document.querySelector("tbody").lastChild as HTMLElement).querySelector("a").getAttribute("href"),
+			title: (document.querySelector("tbody").lastChild as HTMLElement).querySelector("a").getAttribute("title"),
+		})
+	})
 
-   const data = await page.evaluate(() => {
-      return Promise.resolve({
-         href: document.querySelector('tbody').lastChild.querySelector('a').getAttribute("href"),
-         title: document.querySelector('tbody').lastChild.querySelector('a').getAttribute("title"),
-      })
-   })
+	logger.info("Create object with the last children")
+	logger.info(data)
 
-   logger.info('Create object with the last children')
-   logger.info(data)
+	await page.click(`a[title="${data.title}"]`)
+	await waitUntilDownload(page)
 
-   await page.click(`a[title="${data.title}"]`)
-   await waitUntilDownload(page);
+	if (fileName === "PriceFull7290058140886-029") await page.screenshot({ path: "screenshot/ramiLeviPrice.png", fullPage: true })
+	if (fileName === "PromoFull7290058140886-029") await page.screenshot({ path: "screenshot/ramiLeviPromo.png", fullPage: true })
+	logger.info("Screenshot created successfully")
 
-   if (fileName === 'PriceFull7290058140886-029') await page.screenshot({ path: 'ramiLeviPrice.png', fullPage: true })
-   if (fileName === 'PromoFull7290058140886-029') await page.screenshot({ path: 'ramiLeviPromo.png', fullPage: true })
-   logger.info('Screenshot created successfully')
+	await browser.close()
 
-   await browser.close()
-
-   return data
+	return data
 }
 
-const waitUntilDownload = async (page, fileName = '') => {
-   return new Promise((resolve, reject) => {
-      page._client().on('Page.downloadProgress', e => {
-         if (e.state === 'completed') {
-            logger.info('Successfully downloaded');
-            logger.info(e);
-            resolve(fileName)
-         } else if (e.state === 'canceled') {
-            reject()
-         }
-      })
-   })
-}
-
-const getRamiLeviFile = async (path) => {
-
-   const filePath = `./files/${path.title}`
-   const URL = path.href + path.title
-
-   // console.log('File Path: ', filePath)
-   // console.log('Full URL: ', 'https://url.retail.publishedprices.co.il' + path.href)
-
-   try {
-      const file = fs.createWriteStream(filePath)
-      // const request = https.get(URL, (response) => {
-      //    response.pipe(file)
-      // })
-
-      // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-      console.log(URL);
-      request.get(URL)
-         .on('error', (err) => { console.error('******** error: ********', err) })
-         .pipe(file)
-
-      console.log('File SAVED')
-   } catch (error) {
-      console.log('******** ERROR: ********', error)
-   }
-
-}
-
-const renameFile = () => {
-
-   const folderPath = "./files";
-
-   let filesArr = fs.readdirSync(folderPath);
-   filesArr.forEach(file => {
-
-      let fullPath = path.join(folderPath, file);
-      let fileExtension = path.extname(file);
-      let fileName = path.basename(file, fileExtension);
-
-      if (fileExtension === '.crdownload') {
-         console.log(file);
-         console.log(path.extname(file));
-         let newFileName = fileName;
-         try {
-            fs.renameSync(fullPath, path.join(folderPath, newFileName));
-         } catch (error) {
-            console.error(error)
-         }
-      }
-
-   })
-}
-
-const delay = (time) => {
-   return new Promise(function (resolve) {
-      setTimeout(resolve, time)
-   });
+const waitUntilDownload = async (page: any, fileName = "") => {
+	return new Promise((resolve, reject) => {
+		page._client().on("Page.downloadProgress", e => {
+			if (e.state === "completed") {
+				logger.info("Successfully downloaded")
+				logger.info(e)
+				resolve(fileName)
+			} else if (e.state === "canceled") {
+				reject()
+			}
+		})
+	})
 }
 
 // Implement
 const getRamiLevi = async (fileName: string) => {
-   logger.info('====================================')
-   logger.info('Start RamiLevi process')
-   logger.info('====================================')
-   return await getRamiLeviPuppeteer(fileName)
+	logger.info("====================================")
+	logger.info("Start RamiLevi process")
+	logger.info("====================================")
+	return await getRamiLeviPuppeteer(fileName)
 }
 
 module.exports = {
-   getRamiLevi
+	getRamiLevi,
 }
